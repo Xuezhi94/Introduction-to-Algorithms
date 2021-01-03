155242a0872774a297e3e1e8abd8835f690a4355
George Chen <1821883195@qq.com>
2021-01-03 21:34:47 +0800

    insertion sort

---
+++ 2.算法基础/插入排序.py
@@ -0,0 +1,16 @@
+def insertion_sort(l):
+    for i in range(1,len(l)):
+        key = l[i]
+        j = i-1
+        while j >= 0 and l[j] > key:
+            l[j+1] = l[j]
+            j -= 1
+        l[j+1] = key
+
+
+l = [5,2,4,6,1,3]
+insertion_sort(l)
+print(l)
+ 
+
+
